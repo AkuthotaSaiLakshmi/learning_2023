829dcf06bde2ff8954a2daeb318a57cedaf81b5c
Sai <sailakshmiakuthota@gmail.com>
2023-06-21 17:03:21 +0530

    commit 5 : main : Day5 Activities added

---
+++ Module1/Day5/Level1/area of box.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+struct Box {
+    float length;
+    float width;
+    float height;
+};
+float calculateVolume(struct Box* boxPtr) {
+    return (*boxPtr).length * (*boxPtr).width * (*boxPtr).height;
+}
+float calculateSurfaceArea(struct Box* boxPtr) {
+    return 2 * (boxPtr->length * boxPtr->width + boxPtr->length * boxPtr->height + boxPtr->width * boxPtr->height);
+}
+
+int main() {
+    struct Box myBox;
+    struct Box* boxPtr = &myBox;
+    printf("Enter the length, width, and height of the box: ");
+    scanf("%f %f %f", &(myBox.length), &(myBox.width), &(myBox.height));
+    float volume = calculateVolume(boxPtr);
+    float surfaceArea = calculateSurfaceArea(boxPtr);
+    printf("Volume of the box : %.2f\n", volume);
+    printf("Total Surface Area of the box : %.2f\n", surfaceArea);
+    return 0;
+}
---
+++ Module1/Day5/Level1/timeperiod.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+struct Time {
+    int hours;
+    int minutes;
+    int seconds;
+};
+
+// Function to calculate difference between two time periods
+struct Time calculateTimeDifference(struct Time startTime, struct Time endTime) {
+    struct Time difference;
+    int startSeconds = startTime.hours * 3600 + startTime.minutes * 60 + startTime.seconds;
+    int endSeconds = endTime.hours * 3600 + endTime.minutes * 60 + endTime.seconds;
+    int diffSeconds = endSeconds - startSeconds;
+    difference.hours = diffSeconds / 3600;
+    difference.minutes = (diffSeconds % 3600) / 60; 
+    difference.seconds = (diffSeconds % 3600) % 60; 
+
+    return difference;
+}
+
+int main() {
+    struct Time startTime, endTime, difference;
+    printf("Enter the start time (hours minutes seconds): ");
+    scanf("%d %d %d", &(startTime.hours), &(startTime.minutes), &(startTime.seconds));
+    printf("Enter the end time (hours minutes seconds): ");
+    scanf("%d %d %d", &(endTime.hours), &(endTime.minutes), &(endTime.seconds));
+    difference = calculateTimeDifference(startTime, endTime);
+    printf("Time difference: %d hours %d minutes %d seconds\n", difference.hours, difference.minutes, difference.seconds);
+    return 0;
+}
