8ba7c0513da04af179bc05ac07b8638e08fe73a9
Sai <sailakshmiakuthota@gmail.com>
2023-06-26 15:37:31 +0530

    commit 7 : main : Day7 Activities added

---
+++ Module1/Day7/Level1/tempCodeRunnerFile.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+void changeCase(char *str, char option)
+{
+    int i = 0;
+    switch (option)
+    {
+        case 'u':
+            while (str[i])
+            {
+                str[i] = toupper(str[i]);
+                i++;
+            }
+            break;
+        case 'l':
+            while (str[i])
+            {
+                str[i] = tolower(str[i]);
+                i++;
+            }
+            break;
+        case 's':
+            str[0] = toupper(str[0]);
+            while (str[i])
+            {
+                if (str[i] == ' ' || str[i] == '\n' || str[i] == '\t')
+                {
+                    str[i + 1] = toupper(str[i + 1]);
+                }
+                else
+                {
+                    str[i + 1] = tolower(str[i + 1]);
+                }
+                i++;
+            }
+            break;
+        default:
+            break;
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc < 3)
+    {
+        printf("Insufficient arguments.\n");
+        printf("Usage: ./cp <option> <source_file> <destination_file>\n");
+        return 1;
+    }
+
+    char option = 'n'; // 'n' represents no option passed
+    if (argc > 3)
+    {
+        option = argv[1][1];
+    }
+
+    FILE *sourceFile = fopen(argv[2], "r");
+    FILE *destinationFile = fopen(argv[3], "w");
+
+    if (!sourceFile)
+    {
+        printf("Unable to open the source file.\n");
+        return 1;
+    }
